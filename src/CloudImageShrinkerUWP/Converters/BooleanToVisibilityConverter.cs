5d3fd0fd1fd6661c44481f5f1c6fe8a1158e2d4a
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2023-09-18 19:55:24 +0200

    Display processing text

---
+++ src/CloudImageShrinkerUWP/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace CloudImageShrinkerUWP.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (bool.TryParse(value?.ToString(), out bool converted))
+            {
+                return converted ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
